179dcca2f970cef6de0873f3872ac263f4acb525
Tawfiq Obeid <ttawfiq.obeid@gmail.com>
2023-10-05 06:57:51 -0700

    DONE

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,16 @@
 #include <stdio.h>
-int main(void) { puts("Programming is like building a multilingual puzzle"); return (0); }
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints a specific text followed by a newline character
+ * to the standard output.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
+
