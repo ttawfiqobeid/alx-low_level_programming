451a0fb3d08c99fb1a7039d00314665fcdb7f03f
Tawfiq Obeid <ttawfiq.obeid@gmail.com>
2023-10-05 07:25:29 -0700

    DONE

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,9 +8,11 @@
  *
  * Return: Always 0 (Success)
  */
+
+#include <stdio.h>
+
 int main(void)
 {
-	puts(""Programming is like building a multilingual puzzle");
-	return (0);
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
 }
-
