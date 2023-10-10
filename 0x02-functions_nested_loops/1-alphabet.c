0967488a6a18755ee4ee95bd69db4497eef07141
Tawfiq Obeid <ttawfiq.obeid@gmail.com>
2023-10-10 16:26:09 -0700

    done

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the lowercase alphabet followed by a new line
+ */
+void print_alphabet(void)
+{
+_putchar('a');
+_putchar('b');
+_putchar('c');
+_putchar('d');
+_putchar('e');
+_putchar('f');
+_putchar('g');
+_putchar('h');
+_putchar('i');
+_putchar('j');
+_putchar('k');
+_putchar('l');
+_putchar('m');
+_putchar('n');
+_putchar('o');
+_putchar('p');
+_putchar('q');
+_putchar('r');
+_putchar('s');
+_putchar('t');
+_putchar('u');
+_putchar('v');
+_putchar('w');
+_putchar('x');
+_putchar('y');
+_putchar('z');
+_putchar('\n');
+}
