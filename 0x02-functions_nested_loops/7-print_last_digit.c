2592d2a6b87dbf9324e03dd8c6d86a8e9a6e9bb4
Tawfiq Obeid <ttawfiq.obeid@gmail.com>
2023-10-10 17:08:39 -0700

    done

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,10 +6,9 @@
  *
  * Return: The last digit of the number
  */
-int print_last_digit(int n)
+void print_last_digit(int n)
 {
 int last_digit;
-
 if (n < 0)
 {
 last_digit = -n % 10;
@@ -18,8 +17,5 @@ else
 {
 last_digit = n % 10;
 }
-
 _putchar(last_digit + '0');
-
-return (last_digit);
 }
